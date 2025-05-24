2d9f601a6ee81b51666364e76cc3799304fbcc36
yannick Namboure <yanoboston@gmail.com>
2025-05-24 03:57:14 +0000

    modification

--- backend/src/config/database.js
+++ backend/src/config/database.js
@@ -6,9 +6,9 @@ dotenv.config();
 const sequelize = new Sequelize(
   process.env.DB_NAME || 'eventDB',
   process.env.DB_USER || 'postgres',
-  process.env.DB_PASSWORD || 'password',
+  process.env.DB_PASSWORD || 'yano',
   {
-    host: process.env.DB_HOST || 'localhost',
+    host: process.env.DB_HOST || 'db',
     dialect: 'postgres',
     logging: false,
     pool: {
